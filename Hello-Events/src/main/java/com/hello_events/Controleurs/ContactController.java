c4e8485e42069615867975871e5a3ea707423dfa
hajar ouadi <ouadihajar2002@gmail.com>
2024-08-03 01:47:05 +0200

    update contact controller

--- Hello-Events/src/main/java/com/hello_events/Controleurs/ContactController.java
+++ Hello-Events/src/main/java/com/hello_events/Controleurs/ContactController.java
@@ -1,13 +1,20 @@
 package com.hello_events.Controleurs;
 
+import com.hello_events.DTO.ContactDTO;
+import com.hello_events.ResourceNotFoundException;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 import com.hello_events.Entites.Contact;
 import com.hello_events.Services.ContactService;
 
 import java.util.List;
 import java.util.Optional;
+@CrossOrigin(origins = "http://localhost:4200")
 
 @RestController
 @RequestMapping("/contacts")
@@ -15,10 +22,16 @@ public class ContactController {
 
     @Autowired
     private ContactService contactService;
-    @PreAuthorize("hasRole('ADMIN')")
+    @PreAuthorize("hasRole('USER')")
     @PostMapping
-    public Contact createContact(@RequestParam String name, @RequestParam String email, @RequestParam String subject, @RequestParam String message) {
-        return contactService.saveContact(name, email, subject, message);
+    public ResponseEntity<Contact> createContact(
+            @Valid @RequestBody ContactDTO contactDTO, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+
+        Contact contact = contactService.saveContact(contactDTO.getName(), contactDTO.getEmail(), contactDTO.getSubject(), contactDTO.getMessage());
+        return new ResponseEntity<>(contact, HttpStatus.CREATED);
     }
     @PreAuthorize("hasRole('USER')  or  hasRole('ADMIN')" )
     @GetMapping
@@ -32,13 +45,20 @@ public class ContactController {
         return contactService.getContactById(id);
     }
 
-    @PreAuthorize("hasRole('ADMIN')")
-    @PutMapping("/{id}/status")
-    public Contact updateContactStatus(@PathVariable Long id, @RequestParam Contact.ContactStatus status) {
-        return contactService.updateContactStatus(id, status);
 
+    @PreAuthorize("hasRole('USER')")
+    @PatchMapping("/{id}/status")
+    public ResponseEntity<Contact> updateContactStatus(
+            @PathVariable Long id,
+            @RequestParam("status") Contact.ContactStatus status) {
+        try {
+            Contact updatedContact = contactService.updateContactStatus(id, status);
+            return new ResponseEntity<>(updatedContact, HttpStatus.OK);
+        } catch (ResourceNotFoundException e) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
     }
-    @PreAuthorize("hasRole('ADMIN')")
+    @PreAuthorize("hasRole('USER')  or  hasRole('ADMIN')" )
     @DeleteMapping("/{id}")
     public void deleteContact(@PathVariable Long id) {
         contactService.deleteContact(id);
