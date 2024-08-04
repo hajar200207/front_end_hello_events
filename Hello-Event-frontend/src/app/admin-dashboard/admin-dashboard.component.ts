3ed046522195557045953a2bc711079e5a93bf84
chaimaaenaa <mekayssichaimaaenaa@gmail.com>
2024-08-04 14:48:03 +0100

    modify in  front in dashboard admin

--- Hello-Event-frontend/src/app/admin-dashboard/admin-dashboard.component.ts
+++ Hello-Event-frontend/src/app/admin-dashboard/admin-dashboard.component.ts
@@ -15,7 +15,7 @@ interface Event {
 @Component({
   selector: 'app-admin-dashboard',
   templateUrl: './admin-dashboard.component.html',
-  styleUrls: ['./admin-dashboard.component.css']
+  styleUrls: ['./admin-dashboard.component.scss']
 })
 export class AdminDashboardComponent implements OnInit {
   userInfo: any;
